86d8f51e06630ec52cf9dfcef7900ee1cdb24d66
ankit <ankitkumar748846@gmail.com>
2024-04-21 22:14:25 +0530

    Added edit profile page

--- client/src/Redux/Slices/AuthSlice.js
+++ client/src/Redux/Slices/AuthSlice.js
@@ -1,11 +1,15 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import toast from "react-hot-toast";
 import axiosInstance from "../../Helpers/axiosInstance";
+import { json } from "react-router-dom";
 
 const initialState = {
   isLoggedIn: localStorage.getItem("isLoggedIn") || false,
   role: localStorage.getItem("role") || "",
-  data: JSON.parse(localStorage.getItem("data")) || {},
+  data:
+    localStorage.getItem("data") == undefined
+      ? JSON.parse(localStorage.getItem("data"))
+      : {},
 };
 
 export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
@@ -56,6 +60,34 @@ export const logout = createAsyncThunk("/auth/logout", async () => {
   }
 });
 
+export const updateProfile = createAsyncThunk(
+  "/user/update/profile",
+  async (data) => {
+    try {
+      const res = axiosInstance.put(`user/update/${data[0]}`, data[1]);
+      toast.promise(res, {
+        loading: "Wait! profile update in progress...",
+        success: (data) => {
+          return data?.data?.message;
+        },
+        error: "Failed to update profile. Please try again.",
+      });
+      return (await res).data;
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  }
+);
+
+export const getUserData = createAsyncThunk("/user/details", async () => {
+  try {
+    const res = axiosInstance.get("user/me");
+    return (await res).data;
+  } catch (error) {
+    toast.error(error.message);
+  }
+});
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -75,6 +107,15 @@ const authSlice = createSlice({
         state.data = {};
         state.isLoggedIn = false;
         state.role = "";
+      })
+      .addCase(getUserData.fulfilled, (state, action) => {
+        if (!action?.payload?.user) return;
+        localStorage.setItem("data", JSON.stringify(action?.payload?.user));
+        localStorage.setItem("isLoggedIn", true);
+        localStorage.setItem("role", action?.payload?.user?.role);
+        state.isLoggedIn = true;
+        state.data = action?.payload?.user;
+        state.role = action?.payload?.user?.role;
       });
   },
 });
