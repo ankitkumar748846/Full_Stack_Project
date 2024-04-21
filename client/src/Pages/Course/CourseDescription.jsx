7706b3917732a4d6ba6e480a32e0de6b49523569
ankit <ankitkumar748846@gmail.com>
2024-04-22 01:09:39 +0530

    Added complete checkout success logic

--- client/src/Pages/Course/CourseDescription.jsx
+++ client/src/Pages/Course/CourseDescription.jsx
@@ -1,10 +1,12 @@
-import { useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import HomeLayout from "../../Layouts/HomeLayout";
 import { useSelector } from "react-redux";
 
 function CourseDescription() {
   const { state } = useLocation();
-  const { role } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  const { role, data } = useSelector((state) => state.auth);
 
   return (
     <HomeLayout>
@@ -38,7 +40,10 @@ function CourseDescription() {
                   Watch lectures
                 </button>
               ) : (
-                <button className="bg-yellow-600 text-xl rounded-md font-bold px-5 py-3 w-full hover:bg-yellow-500 transition-all ease-in-out duration-300">
+                <button
+                  onClick={() => navigate("/checkout")}
+                  className="bg-yellow-600 text-xl rounded-md font-bold px-5 py-3 w-full hover:bg-yellow-500 transition-all ease-in-out duration-300"
+                >
                   Subscribe
                 </button>
               )}
--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from "./Slices/AuthSlice";
 import courseSliceReducer from "./Slices/CourseSlice";
+import razorpaySliceReducer from "./Slices/RazorpaySlice";
 const store = configureStore({
-    reducer: {
-        auth : authSliceReducer,
-        course:courseSliceReducer
-    },
-    devTools:true
+  reducer: {
+    auth: authSliceReducer,
+    course: courseSliceReducer,
+    razorpay: razorpaySliceReducer
+  },
+  devTools: true,
 });
 
 export default store;
